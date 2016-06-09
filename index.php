c7e82ea4f43147487fcb1cc2489b53e050903d2d
Tavis <tavis@tavisonline.com>
2016-06-09 17:54:36 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+ 
+<head> 
+	<title>Holy Qur'an: Arabic with parallel English translation</title>
+	<meta name="description" content="Side-by-side Arabic Holy Qur'an with English translation by Yusuf Ali" /> 
+	<meta name="keywords" content="Quran Text, parallel, side-by-side, Verified, Qur'an, Al-Qur'an, Kuran, Koran, Translation, one ummah,  قرائت ,قرآن كريم ,ترجمه,دقيق" /> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+	<link href="book/styles/arabic-style.css" type="text/css" rel="stylesheet" />
+</head>
+<body class="main-body">
+<div id="container">
+<div id="main-header">
+</div>
+
+<div id="contents">
+<p id="fontdownload">For optimum reading, we recommend downlading this <a href="ScheherazadeRegOT.ttf">arabic font</a>.  For a complete Qur'an study tool, download <a href="http://zekr.org/">Zekr</a>.</p>
+
+<p>Help keep our Qur'an section Ad-free.  Please donate today.
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="F8SHNDBGWDHEA">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</p>
+
+<p><a href="http://www.oneummah.net/quran/book/1.html">1. Al-Fatiha / The Opening</a></p>
+<p><a href="http://www.oneummah.net/quran/book/2.html">2. Al-Baqarah / The Cow</a></p>
+<p><a href="http://www.oneummah.net/quran/book/3.html">3. Aal-i-Imran / The Family of Imran</a></p>
+<p><a href="http://www.oneummah.net/quran/book/4.html">4. An-Nisaa / The Women</a></p>
+<p><a href="http://www.oneummah.net/quran/book/5.html">5. Al-Ma'ida / The Table Spread</a></p>
+<p><a href="http://www.oneummah.net/quran/book/6.html">6. Al-An'aam / The Cattle</a></p>
+<p><a href="http://www.oneummah.net/quran/book/7.html">7. Al-A'raaf / The Heights</a></p>
+<p><a href="http://www.oneummah.net/quran/book/8.html">8. Al-Anfaal / The Spoils of War </a></p>
+<p><a href="http://www.oneummah.net/quran/book/9.html">9. At-Tawba / The Repentance </a></p>
+<p><a href="http://www.oneummah.net/quran/book/10.html">10. Yunus / Jonas </a></p>
+<p><a href="http://www.oneummah.net/quran/book/11.html">11. Hud / Hud </a></p>
+<p><a href="http://www.oneummah.net/quran/book/12.html">12. Yusuf / Joseph </a></p>
+<p><a href="http://www.oneummah.net/quran/book/13.html">13. Ar-Ra'd / The Thunder </a></p>
+<p><a href="http://www.oneummah.net/quran/book/14.html">14. Ibrahim / Abraham</a></p>
+<p><a href="http://www.oneummah.net/quran/book/15.html">15. Al-Hijr / The Rock</a></p>
+<p><a href="http://www.oneummah.net/quran/book/16.html">16. An-Nahl / The Bee</a></p>
+<p><a href="http://www.oneummah.net/quran/book/17.html">17. Al-Israa / The Night Journey</a></p>
+<p><a href="http://www.oneummah.net/quran/book/18.html">18. Al-Kahf / The Cave </a></p>
+<p><a href="http://www.oneummah.net/quran/book/19.html">19. Maryam / Mary</a></p>
+<p><a href="http://www.oneummah.net/quran/book/20.html">20. Ta-Ha / Ta-ha</a></p>
+<p><a href="http://www.oneummah.net/quran/book/21.html">21. Al-Anbiyaa / The Prophets</a></p>
+<p><a href="http://www.oneummah.net/quran/book/22.html">22. Al-Hajj / The Pilgrimage</a></p>
+<p><a href="http://www.oneummah.net/quran/book/23.html">23. Al-Muminoon / The Believers </a></p>
+<p><a href="http://www.oneummah.net/quran/book/24.html">24. An-Noor / The Light </a></p>
+<p><a href="http://www.oneummah.net/quran/book/25.html">25. Al-Furqaan / The Criterion</a></p>
+<p><a href="http://www.oneummah.net/quran/book/26.html">26. Ash-Shu'araa / The Poets</a></p>
+<p><a href="http://www.oneummah.net/quran/book/27.html">27. An-Naml / The Ant</a></p>
+<p><a href="http://www.oneummah.net/quran/book/28.html">28. Al-Qasas / The Stories</a></p>
+<p><a href="http://www.oneummah.net/quran/book/29.html">29. Al-Ankaboot / The Spider</a></p>
+<p><a href="http://www.oneummah.net/quran/book/30.html">30. Ar-Room / The Romans</a></p>
+<p><a href="http://www.oneummah.net/quran/book/31.html">31. Luqman / Luqman</a></p>
+<p><a href="http://www.oneummah.net/quran/book/32.html">32. As-Sajdah / The Prostration</a></p>
+<p><a href="http://www.oneummah.net/quran/book/33.html">33. Al-Ahzaab / The Clans</a></p>
+<p><a href="http://www.oneummah.net/quran/book/34.html">34. Saba / Sheba</a></p>
+<p><a href="http://www.oneummah.net/quran/book/35.html">35. Faatir / The Originator</a></p>
+<p><a href="http://www.oneummah.net/quran/book/36.html">36. Yaseen / Yaseen</a></p>
+<p><a href="http://www.oneummah.net/quran/book/37.html">37. As-Saaffaat / Those drawn up in Ranks </a></p>
+<p><a href="http://www.oneummah.net/quran/book/38.html">38. Saad / Saad</a></p>
+<p><a href="http://www.oneummah.net/quran/book/39.html">39. Az-Zumar / The Groups</a></p>
+<p><a href="http://www.oneummah.net/quran/book/40.html">40. Al-Ghaafir / The Forgiver</a></p>
+<p><a href="http://www.oneummah.net/quran/book/41.html">41. Fussilat / Explained in detail </a></p>
+<p><a href="http://www.oneummah.net/quran/book/42.html">42. Ash-Shura / Consultation</a></p>
+<p><a href="http://www.oneummah.net/quran/book/43.html">43. Az-Zukhruf / Ornaments of gold </a></p>
+<p><a href="http://www.oneummah.net/quran/book/44.html">44. Ad-Dukhaan / The Smoke</a></p>
+<p><a href="http://www.oneummah.net/quran/book/45.html">45. Al-Jaathiya / Crouching</a></p>
+<p><a href="http://www.oneummah.net/quran/book/46.html">46. Al-Ahqaf / The Dunes </a></p>
+<p><a href="http://www.oneummah.net/quran/book/47.html">47. Muhammad / Muhammad</a></p>
+<p><a href="http://www.oneummah.net/quran/book/48.html">48. Al-Fath / The Victory</a></p>
+<p><a href="http://www.oneummah.net/quran/book/49.html">49. Al-Hujuraat / The Inner Apartments</a></p>
+<p><a href="http://www.oneummah.net/quran/book/50.html">50. Qaaf / Qaaf</a></p>
+<p><a href="http://www.oneummah.net/quran/book/51.html">51. Adh-Dhaariyat / The Winnowing Winds</a></p>
+<p><a href="http://www.oneummah.net/quran/book/52.html">52. At-Tur / The Mount</a></p>
+<p><a href="http://www.oneummah.net/quran/book/53.html">53. An-Najm / The Star</a></p>
+<p><a href="http://www.oneummah.net/quran/book/54.html">54. Al-Qamar / The Moon</a></p>
+<p><a href="http://www.oneummah.net/quran/book/55.html">55. Ar-Rahman / The Beneficent </a></p>
+<p><a href="http://www.oneummah.net/quran/book/56.html">56. Al-Waaqia / The Inevitable </a></p>
+<p><a href="http://www.oneummah.net/quran/book/57.html">57. Al-Hadid / The Iron</a></p>
+<p><a href="http://www.oneummah.net/quran/book/58.html">58. Al-Mujaadila / The Pleading Woman</a></p>
+<p><a href="http://www.oneummah.net/quran/book/59.html">59. Al-Hashr / The Exile</a></p>
+<p><a href="http://www.oneummah.net/quran/book/60.html">60. Al-Mumtahana / She that is to be examined</a></p>
+<p><a href="http://www.oneummah.net/quran/book/61.html">61. As-Saff / The Ranks</a></p>
+<p><a href="http://www.oneummah.net/quran/book/62.html">62. Al-Jumu'a / Friday</a></p>
+<p><a href="http://www.oneummah.net/quran/book/63.html">63. Al-Munafiqun / The Hypocrites</a></p>
+<p><a href="http://www.oneummah.net/quran/book/64.html">64. At-Taghaabun / Mutual Disillusion</a></p>
+<p><a href="http://www.oneummah.net/quran/book/65.html">65. At-Talaq / Divorce</a></p>
+<p><a href="http://www.oneummah.net/quran/book/66.html">66. At-Tahrim / The Prohibition</a></p>
+<p><a href="http://www.oneummah.net/quran/book/67.html">67. Al-Mulk / The Sovereignty</a></p>
+<p><a href="http://www.oneummah.net/quran/book/68.html">68. Al-Qalam / The Pen</a></p>
+<p><a href="http://www.oneummah.net/quran/book/69.html">69. Al-Haqqah / The Reality</a></p>
+<p><a href="http://www.oneummah.net/quran/book/70.html">70. Al-Ma'arij / The Ascending Stairways</a></p>
+<p><a href="http://www.oneummah.net/quran/book/71.html">71. Nuh / Noah </a></p>
+<p><a href="http://www.oneummah.net/quran/book/72.html">72. Al-Jinn / The Jinn</a></p>
+<p><a href="http://www.oneummah.net/quran/book/73.html">73. Al-Muzzammil / The Enshrouded One</a></p>
+<p><a href="http://www.oneummah.net/quran/book/74.html">74. Al-Muddaththir / The Cloaked one</a></p>
+<p><a href="http://www.oneummah.net/quran/book/75.html">75. Al-Qiyamah / The Resurrection</a></p>
+<p><a href="http://www.oneummah.net/quran/book/76.html">76. Al-Insan / Man</a></p>
+<p><a href="http://www.oneummah.net/quran/book/77.html">77. Al-Mursalat / The Emissaries</a></p>
+<p><a href="http://www.oneummah.net/quran/book/78.html">78. An-Naba / The Announcement</a></p>
+<p><a href="http://www.oneummah.net/quran/book/79.html">79. An-Nazi'at / Those who drag forth</a></p>
+<p><a href="http://www.oneummah.net/quran/book/80.html">80. Abasa / He frowned</a></p>
+<p><a href="http://www.oneummah.net/quran/book/81.html">81. At-Takwir / The Overthrowing</a></p>
+<p><a href="http://www.oneummah.net/quran/book/82.html">82. Al-Infitar / The Cleaving</a></p>
+<p><a href="http://www.oneummah.net/quran/book/83.html">83. Al-Mutaffifin / Defrauding</a></p>
+<p><a href="http://www.oneummah.net/quran/book/84.html">84. Al-Inshiqaq / The Splitting Open</a></p>
+<p><a href="http://www.oneummah.net/quran/book/85.html">85. Al-Buruj / The Constellations</a></p>
+<p><a href="http://www.oneummah.net/quran/book/86.html">86. At-Tariq / The Morning Star</a></p>
+<p><a href="http://www.oneummah.net/quran/book/87.html">87. Al-A'la / The Most High</a></p>
+<p><a href="http://www.oneummah.net/quran/book/88.html">88. Al-Ghaashiya / The Overwhelming </a></p>
+<p><a href="http://www.oneummah.net/quran/book/89.html">89. Al-Fajr / The Dawn</a></p>
+<p><a href="http://www.oneummah.net/quran/book/90.html">90. Al-Balad / The City</a></p>
+<p><a href="http://www.oneummah.net/quran/book/91.html">91. Ash-Shams / The Sun</a></p>
+<p><a href="http://www.oneummah.net/quran/book/92.html">92. Al-Lail / The Night </a></p>
+<p><a href="http://www.oneummah.net/quran/book/93.html">93. Ad-Dhuha / The Morning Hours</a></p>
+<p><a href="http://www.oneummah.net/quran/book/94.html">94. Ash-Sharh / The Consolation</a></p>
+<p><a href="http://www.oneummah.net/quran/book/95.html">95. At-Tin / The Fig</a></p>
+<p><a href="http://www.oneummah.net/quran/book/96.html">96. Al-Alaq / The Clot</a></p>
+<p><a href="http://www.oneummah.net/quran/book/97.html">97. Al-Qadr / The Power, Fate</a></p>
+<p><a href="http://www.oneummah.net/quran/book/98.html">98. Al-Bayyina / The Evidence </a></p>
+<p><a href="http://www.oneummah.net/quran/book/99.html">99. Az-Zalzala / The Earthquake</a></p>
+<p><a href="http://www.oneummah.net/quran/book/100.html">100. Al-Adiyat / The Chargers</a></p>
+<p><a href="http://www.oneummah.net/quran/book/101.html">101. Al-Qari'a / The Calamity</a></p>
+<p><a href="http://www.oneummah.net/quran/book/102.html">102. At-Takathur / Competition</a></p>
+<p><a href="http://www.oneummah.net/quran/book/103.html">103. Al-Asr / The Declining Day, Epoch</a></p>
+<p><a href="http://www.oneummah.net/quran/book/104.html">104. Al-Humaza / The Traducer</a></p>
+<p><a href="http://www.oneummah.net/quran/book/105.html">105. Al-Fil / The Elephant</a></p>
+<p><a href="http://www.oneummah.net/quran/book/106.html">106. Quraysh / Quraysh</a></p>
+<p><a href="http://www.oneummah.net/quran/book/107.html">107. Al-Ma'un / Almsgiving</a></p>
+<p><a href="http://www.oneummah.net/quran/book/108.html">108. Al-Kawthar / Abundance</a></p>
+<p><a href="http://www.oneummah.net/quran/book/109.html">109. Al-Kafirun / The Disbelievers</a></p>
+<p><a href="http://www.oneummah.net/quran/book/110.html">110. An-Nasr / Divine Support</a></p>
+<p><a href="http://www.oneummah.net/quran/book/111.html">111. Al-Masad / The Palm Fibre</a></p>
+<p><a href="http://www.oneummah.net/quran/book/112.html">112. Al-Ikhlas / Sincerity</a></p>
+<p><a href="http://www.oneummah.net/quran/book/113.html">113. Al-Falaq / The Dawn</a></p>
+<p><a href="http://www.oneummah.net/quran/book/114.html">114. An-Naas / Mankind</a></p>
+</div>
+
+<div class="clr"></div>
+<div id="footer">A Project of the Bayt al-Hikmah Digital Library, sponsored by <a href="http://www.oneummah.net">One Ummah Network</a>.<br />
+Arabic text provided by <a href="http://tanzil.info/wiki/Tanzil_Project">The Tanzil Project</a> and released under a <a href="http://creativecommons.org/licenses/by-nc-nd/3.0/">Creative Commons</a> License.<br />
+Translation provided by <a href="http://qurandatabase.org/">Quran Database</a>.
+</div>
+</div>
